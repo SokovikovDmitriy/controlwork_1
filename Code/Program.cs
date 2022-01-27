af6612c998208399700d0779b7d5d41d9e2090fd
dmitriy_s <d.a.sokovikov@gmail.com>
2022-01-27 15:31:59 +0300

    add File Code

---
+++ Code/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+
+String[] array = { "i", "am", "not", "a", "programist", "yet" };
+String[] newArray = new String[array.Length];
+int j = 0;
+
+Console.Write("[");
+for (int i = 0; i < array.Length; i++)
+    Console.Write(array[i] + " ");
+
+Console.Write("] => ");
+Console.Write("[");
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length < 4)
+    {
+        newArray[j] = array[i];
+        j++;
+    }
+}
+
+for (int i = 0; i < newArray.Length; i++)
+    Console.Write(newArray[i] + " ");
+Console.Write("]");
